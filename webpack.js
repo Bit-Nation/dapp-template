d8621bc84347d059bbc39a3be1fbeca8f047e76c
Hoang Hung <hth321@gmail.com>
2018-06-21 06:55:29 +0700

    implement webpack watch mode

---
+++ webpack.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const webpack = require('webpack');
+const fs = require('fs');
+
+const writeFile = () => {
+  const filePath = path.join(__dirname, 'dist/index.js');
+  const outputPath = path.join(__dirname, 'build.json');
+  const obj = JSON.parse(
+    fs.readFileSync(path.join(__dirname, 'dapp_config.json'), 'utf8')
+  );
+  const jsonData = obj ? obj : {};
+  fs.readFile(filePath, { encoding: 'utf-8' }, function(err, data) {
+    if (!err) {
+      const content = JSON.stringify({
+        ...jsonData,
+        code: data,
+      });
+      fs.writeFile(outputPath, content, 'utf8', function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log('success write update');
+      });
+    } else {
+      console.log(err);
+    }
+  });
+};
+
+const mode = process.argv[2];
+const compiler = webpack({
+  entry: ['babel-polyfill', './main.js'],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: mode ? mode : 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+});
+
+const watching = compiler.watch(
+  {
+    // Example watchOptions
+    aggregateTimeout: 300,
+    poll: undefined,
+  },
+  (err, stats) => {
+    // Print watch/build result here...
+    writeFile();
+    if (mode != 'development') {
+      watching.close();
+    }
+  }
+);
