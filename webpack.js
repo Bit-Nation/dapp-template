b946db5ae4a7cba6e6fad37da53f810f56808cae
Hoang Hung <hth321@gmail.com>
2018-06-22 14:38:36 +0700

    fix naming issues

--- webpack.js
+++ webpack.js
@@ -1,30 +1,48 @@
 const path = require('path');
 const webpack = require('webpack');
 const fs = require('fs');
-const filePath = path.join(__dirname, 'dist/index.js');
-const outputPath = path.join(__dirname, 'build.json');
-const appIconPath = path.join(__dirname, 'appIcon.png');
 
-const base64_encode = file => {
+/* Helper functions start*/
+const ensureDirectoryExistence = filePath => {
+  const dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+};
+
+const base64Encode = file => {
   // read binary data
   const bitmap = fs.readFileSync(file);
   // convert binary data to base64 encoded string
   return new Buffer(bitmap).toString('base64');
 };
 
-const obj = JSON.parse(
+/* Helper functions end*/
+
+const filePath = path.join(__dirname, 'dist/index.js');
+const outputPath = path.join(__dirname, 'build/build.json');
+const appIconPath = path.join(__dirname, 'appIcon.png');
+
+const mode = process.argv[2]; // get mode argurment from command line
+
+const dappConfig = JSON.parse(
   fs.readFileSync(path.join(__dirname, 'dapp_config.json'), 'utf8')
 );
 
-const jsonData = obj ? { ...obj, image: base64_encode(appIconPath) } : {};
+const dAppMetaData = dappConfig
+  ? { ...dappConfig, image: base64Encode(appIconPath) }
+  : {};
 
-const writeFile = () => {
+const writeBuildFile = () => {
   fs.readFile(filePath, { encoding: 'utf-8' }, function(err, data) {
     if (!err) {
       const content = JSON.stringify({
-        ...jsonData,
+        ...dAppMetaData,
         code: data,
       });
+      ensureDirectoryExistence(outputPath);
       fs.writeFile(outputPath, content, 'utf8', function(err) {
         if (err) {
           return console.log(err);
@@ -37,7 +55,6 @@ const writeFile = () => {
   });
 };
 
-const mode = process.argv[2];
 const compiler = webpack({
   entry: ['babel-polyfill', './app.js'],
   output: {
@@ -66,7 +83,7 @@ const watching = compiler.watch(
   },
   (err, stats) => {
     // Print watch/build result here...
-    writeFile();
+    writeBuildFile();
     if (mode != 'development') {
       watching.close();
     }
