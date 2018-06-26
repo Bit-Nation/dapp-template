af93ebf677dca02d2eff42607e28550bbd4d82f5
Florian Lenz <flenz81@googlemail.com>
2018-06-26 03:49:23 +0300

    [dapp] added a bit of sample code to DApp

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-import React from 'react';
+import pangea from 'pangea-sdk'
+
+const {
+    renderUI,
+    View,
+    Text,
+    setOpenHandler,
+    setMessageHandler,
+    showModal,
+} = pangea;
+
+// this handler will be called
+// when the user opens your DApp
+setOpenHandler((context) => {
+
+    // layout that will be rendered
+    const layout = new View(
+        {},
+        [
+            new Text(
+                {},
+                "Hi there"
+            ),
+            new Text(
+                {},
+                "This is the Pange VM"
+            )
+        ]
+    );
+
+    // show a modal with "Select Action" as the title
+    // and the rendered layout
+    showModal("Select Action", renderUI(layout))
+
+});
+
+// handle message rendering
+setMessageHandler((dAppMessage, context, cb) => {
+
+    if (dAppMessage.type === "SEND_MONEY") {
+
+        // create layout
+        const layout = new View(
+            {},
+            [
+                new Text(
+                    {},
+                    "This is a send money Message"
+                )
+            ]
+        );
+
+        // render layout
+        return cb(null, layout.toJSON())
+
+    }
+
+});
